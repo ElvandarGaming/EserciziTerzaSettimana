1c0297b6b86e4af4b071259fcc4b11e2dabc7866
binot <binot@LAPTOP-6R8KPB6L.station>
2021-06-28 17:25:29 +0200

    esercizio 2 completo, anche con double

--- EserciziTerzaSettimana/src/eccezioniCalcoloKmLitro/CalcoloKmLitro.java
+++ EserciziTerzaSettimana/src/eccezioniCalcoloKmLitro/CalcoloKmLitro.java
@@ -4,15 +4,25 @@ public class CalcoloKmLitro {
 
 	public static void main(String[] args) {
 
-		
-		System.out.println(calcolaKmLitro(52,8));
-		
-		
+		try {
+			System.out.println(calcolaKmLitro(100, 26));
+		} catch (DivideByZero e) {
+			System.out.println(e.getMessage());
+		} catch (ArithmeticException e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 
-	public static double calcolaKmLitro(double km, double litri) {
-		return km/litri;
+	public static double calcolaKmLitro(double km, double litri) throws DivideByZero {
+		if (litri == 0) {
+			throw new DivideByZero("Un pò di benza l'avrai usata");
+		}
+		return km / litri;
 	}
-	
-	
+
+	public static double calcolaKmLitro(int km, int litri) throws DivideByZero {
+		return km / litri;
+	}
+
 }
---
+++ EserciziTerzaSettimana/src/eccezioniCalcoloKmLitro/DivideByZero.java
@@ -0,0 +1,9 @@
+package eccezioniCalcoloKmLitro;
+
+public class DivideByZero extends Exception {
+
+	public DivideByZero(String message) {
+		super(message);
+	}
+
+}
