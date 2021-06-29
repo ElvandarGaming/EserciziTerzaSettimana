25a193703137a6f36a86f0e053fe9d66a9f8c041
binot <binot@LAPTOP-6R8KPB6L.station>
2021-06-29 20:12:14 +0200

    esercizio 3 completo con main di test

--- EserciziTerzaSettimana/src/esercizioHashMap/Start.java
+++ EserciziTerzaSettimana/src/esercizioHashMap/Start.java
@@ -6,19 +6,24 @@ public class Start {
 		
 		RubricaTelefonica mix = new RubricaTelefonica();
 		
+		//1
 		mix.addContact("pippo", "049953571");
 		mix.addContact("pippa", "049953572");
 		mix.addContact("pippi", "049953573");
 		mix.addContact("pippe", "049953574");
 		mix.addContact("pippu", "049953575");
-		
+		//5
 		mix.printRubrica();
-		
+		//2
 		mix.removeContact("pippi");
-		
 		System.out.println();
 		mix.printRubrica();
-		
+		//3
+		System.out.println();
+		System.out.println(mix.searchName("049953574"));
+		//4
+		System.out.println();
+		System.out.println(mix.searchNumber("pippo"));
 		
 		
 	}
