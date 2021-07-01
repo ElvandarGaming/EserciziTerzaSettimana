b835df31d8d2af3624ea6c734478212293db8c14
binot <binot@LAPTOP-6R8KPB6L.station>
2021-07-02 00:10:30 +0200

    HashSet

--- EserciziTerzaSettimana/src/coreJava/CoreJavaTest.java
+++ EserciziTerzaSettimana/src/coreJava/CoreJavaTest.java
@@ -1,9 +1,12 @@
 package coreJava;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CoreJavaTest {
 
 	public static void main(String[] args) {
-
+		Set<String> f = new HashSet<>();
 	}
 
 }
