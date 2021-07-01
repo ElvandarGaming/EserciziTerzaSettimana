e1873105fb56f617da79e08bd43fa38c6ca2d518
binot <binot@LAPTOP-6R8KPB6L.station>
2021-07-01 22:53:44 +0200

    eliminazione warning

--- EserciziTerzaSettimana/src/esercizioHashMap/RubricaTelefonica.java
+++ EserciziTerzaSettimana/src/esercizioHashMap/RubricaTelefonica.java
@@ -1,41 +1,41 @@
 package esercizioHashMap;
 
-import java.util.HashMap;
+//import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class RubricaTelefonica {
-	
-	//private Map<String,String> contatti = new HashMap<>();
-	private Map<String,String> contatti = new TreeMap<>();
+
+	// private Map<String,String> contatti = new HashMap<>();
+	private Map<String, String> contatti = new TreeMap<>();
 
 	public void addContact(String nome, String numero) {
 		contatti.put(nome, numero);
 	}
-	
+
 	public void removeContact(String nome) {
 		contatti.remove(nome);
 	}
-	
+
 	public String searchName(String numero) {
-		
-		for(Map.Entry<String,String> num :contatti.entrySet()) {
-			if(num.getValue().equals(numero)) {
+
+		for (Map.Entry<String, String> num : contatti.entrySet()) {
+			if (num.getValue().equals(numero)) {
 				return num.getKey();
 			}
 		}
 		return "Numero non presente";
 	}
-	
+
 	public String searchNumber(String nome) {
 		return contatti.get(nome);
 	}
-	
+
 	public void printRubrica() {
-		for(Map.Entry<String,String> f : contatti.entrySet()) {
+		for (Map.Entry<String, String> f : contatti.entrySet()) {
 			System.out.print(f.getKey() + " ");
 			System.out.println(f.getValue());
 		}
 	}
-	
+
 }
